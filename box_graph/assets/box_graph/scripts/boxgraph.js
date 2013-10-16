d59acaacc809c8ae107de43e273db511ddac3270
Eric Tsai <erictsai6@gmail.com>
2013-10-15 23:20:35 -0400

    Finished up styling of the box graph

--- box_graph/assets/box_graph/scripts/boxgraph.js
+++ box_graph/assets/box_graph/scripts/boxgraph.js
@@ -1,23 +1,20 @@
 (function() {
 
-// Inspired by http://informationandvisualization.de/blog/box-plot
 d3.boxgraph = function() {
   var width = 1,
       height = 1,
       duration = 0,
       domain = null,
       value = Number,
+      x = undefined,
+      y = undefined,
+      xaxis = undefined,
+      yaxis = undefined,
+      margin = {'top': 15, 'bottom': 15, 'left': 40, 'right': 50},
       box_width = 1,
-      x = d3.scale.linear()
-        .range([0, 650]),
-      y = d3.scale.linear()
-        .domain([0, 100])
-        .range([300, 0]),
-      xaxis = d3.svg.axis().scale(x).orient("bottom"),
-      yaxis = d3.svg.axis().scale(y).orient("left"),
-      margin = {'top': 10, 'bottom': 10, 'left': 30, 'right': 30},
+      box_padding = 15,
       tickFormat = null,
-      data=null;
+      data = undefined;
 
   // For each small multiple…
   function boxgraph(el) {
@@ -26,17 +23,36 @@ d3.boxgraph = function() {
       // Raise an error
       throw Exception("No element found");
     }
+    if (height == undefined || width == undefined ||
+          data == undefined) {
+      throw Exception("Uninitialized height, width or data");
+    }
     var adjusted_height = height - margin.top - margin.bottom,
-        adjusted_width = width - margin.left - margin.right;
-    x = d3.scale.linear()
-      .range([0, adjusted_width])
-      .domain([0, 12]),
+        adjusted_width = width - margin.left - margin.right,
+        x_ticks = [new Date(2000, 0), new Date(2000, 1),
+                   new Date(2000, 2), new Date(2000, 3),
+                   new Date(2000, 4), new Date(2000, 5),
+                   new Date(2000, 6), new Date(2000, 7),
+                   new Date(2000, 8), new Date(2000, 9),
+                   new Date(2000, 10), new Date(2000, 11)],
+        x_scale = [new Date(2000,0,1), new Date(2000,11,31)];
+
+    x = d3.time.scale()
+          .domain(x_scale)
+          .range([0, adjusted_width]);
     y = d3.scale.linear()
-      .domain([0, 100])
-      .range([adjusted_height, 0]),
-    xaxis = d3.svg.axis().scale(x).orient("bottom"),
-    yaxis = d3.svg.axis().scale(y).orient("left"),
-    box_width = width / data.length;
+          .domain(domain)
+          .range([adjusted_height, 0]);
+    xaxis = d3.svg.axis()
+          .scale(x)
+          .orient('bottom')
+          .ticks(d3.time.months, 1)
+          .tickFormat(d3.time.format('%B'))
+          .tickSize(5,10)
+          .tickPadding(0,15);
+    yaxis = d3.svg.axis().scale(y).orient("left").tickFormat(formatDegrees);
+    box_width = adjusted_width / data.length - box_padding * 2;
+
     el.each(function() {
       var div = d3.select(this),
           g = div.select("g");
@@ -45,14 +61,17 @@ d3.boxgraph = function() {
       if (g.empty()) {
         g = div
           .append("svg")
-            .attr("width", width)
-            .attr("height", height)
+            .attr("viewBox", "0 0 "+width+" "+height)
+            .attr("preserveAspectRatio", "none");
         g.append("g")
-          .attr("class", "bg-xaxis")
-          .attr("transform", "translate(" + margin.left + "," + (adjusted_height) + ")")
-          .call(xaxis);
+          .attr("class", "bg-xaxis bg-axis")
+          .attr("transform", "translate(" + margin.left + "," + (adjusted_height + margin.top) + ")")
+          .call(xaxis)
+        .selectAll(".tick text")
+          .style("text-anchor", "start")
+          .attr("x", 10);
         g.append("g")
-          .attr("class", "bg-yaxis")
+          .attr("class", "bg-yaxis bg-axis")
           .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
           .call(yaxis);
         g.append("g")
@@ -66,14 +85,25 @@ d3.boxgraph = function() {
           datum = data[i]
           bgChart.append("rect")
             .attr("class", "bg-box")
-            .attr("x", x(i))
-            .attr("y", y(datum[0]))
+            .attr("x", x(x_ticks[i]) + box_padding)
+            .attr("y", y(datum[2]))
             .attr("width", box_width)
-            .attr("height", y(datum[0]) - y(datum[2]))
+            .attr("height", y(datum[0]) - y(datum[2]));
+          bgChart.append("line")
+            .attr("class", "bg-median")
+            .attr("x1", x(x_ticks[i]) + box_padding)
+            .attr("y1", y(datum[1]))
+            .attr("x2", x(x_ticks[i]) + box_padding + box_width)
+            .attr("y2", y(datum[1]));
         }
       }
     });
-    d3.timer.flush();
+  }
+
+  function formatDegrees(d) {
+    return d === y.domain()[1]
+        ? d + "°F"
+        : d;
   }
 
   boxgraph.import_data = function(x) {
@@ -108,7 +138,7 @@ d3.boxgraph = function() {
 
   boxgraph.domain = function(x) {
     if (!arguments.length) return domain;
-    domain = x == null ? x : d3.functor(x);
+    domain = x;
     return boxgraph;
   };
 
@@ -118,36 +148,13 @@ d3.boxgraph = function() {
     return boxgraph;
   };
 
-  boxgraph.whiskers = function(x) {
-    if (!arguments.length) return whiskers;
-    whiskers = x;
-    return boxgraph;
-  };
   boxgraph.margin = function(x) {
     if (!arguments.length) return margin;
     margin = x;
     return boxgraph;
   }
 
-  boxgraph.quartiles = function(x) {
-    if (!arguments.length) return quartiles;
-    quartiles = x;
-    return boxgraph;
-  };
-
   return boxgraph;
 };
-
-function boxWhiskers(d) {
-  return [0, d.length - 1];
-}
-
-function boxQuartiles(d) {
-  return [
-    d3.quantile(d, .25),
-    d3.quantile(d, .5),
-    d3.quantile(d, .75)
-  ];
-}
 
 })();
